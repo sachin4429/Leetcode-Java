e40d38f62f93e5417ab10d9151e24aed4df2b802
sachin4429 <56739056+sachin4429@users.noreply.github.com>
2021-08-13 20:26:09 +0530

    Time: 1 ms (95.08%), Space: 38.6 MB (6.28%) - LeetHub

--- distribute-candies-to-people/distribute-candies-to-people.java
+++ distribute-candies-to-people/distribute-candies-to-people.java
@@ -1,18 +1,18 @@
 class Solution {
-  public int[] distributeCandies(int candies, int n) {
-    int p = (int)(Math.sqrt(2 * candies + 0.25) - 0.5);
-    int remaining = (int)(candies - (p + 1) * p * 0.5);
-    int rows = p / n, cols = p % n;
-
-    int[] d = new int[n];
-    for(int i = 0; i < n; ++i) {
-      // complete rows
-      d[i] = (i + 1) * rows + (int)(rows * (rows - 1) * 0.5) * n;
-      // cols in the last row
-      if (i < cols) d[i] += i + 1 + rows * n;
+    public int[] distributeCandies(int candies, int num_people) {
+        int[] res = new int[num_people];
+        int distribute = 1;
+        int index = 0;
+        while (candies > 0) {
+            if (candies <= distribute) {
+                res[index] += candies;
+                break;
+            } else {
+                res[index] += distribute;
+                candies -= distribute++;
+                index = index == num_people - 1 ? 0 : index + 1;
+            }
+        }
+        return res;
     }
-    // remaining candies        
-    d[cols] += remaining;
-    return d;
-  }
 }
