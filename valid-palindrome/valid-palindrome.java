be1fc87515d33c33560401fd9eb625892377fe83
sachin4429 <56739056+sachin4429@users.noreply.github.com>
2021-08-13 19:44:53 +0530

    Time: 4 ms (49.44%), Space: 40.5 MB (11.87%) - LeetHub

--- valid-palindrome/valid-palindrome.java
+++ valid-palindrome/valid-palindrome.java
@@ -1,24 +1,36 @@
 class Solution {
     public boolean isPalindrome(String s) {
-       String SB=s.toUpperCase();
-        s="";
-        for(int i=0; i<SB.length(); i++)
-		{
-			if((int)SB.charAt(i)>=65 && (int)SB.charAt(i)<=90 || Character.isDigit(SB.charAt(i)))
-			{
-				s = s+SB.charAt(i);
-			}
-		}
-        StringBuilder RSB = new StringBuilder(s);
-        RSB = RSB.reverse();
-        
-        System.out.println(s+" "+RSB.toString());
-       
-        if(s.equals(RSB.toString()))
-            return true;
-        else 
-            return false;
-        
-           
+        int low = 0;
+        int high = s.length() - 1;
+        while (true) {
+            while(low <= high && !isValid(s, low)) {
+                low++;
+            }
+            while (low <= high && !isValid(s, high)) {
+                high--;
+            }
+            if (low <= high) {
+                if (normalize(s.charAt(low)) != normalize(s.charAt(high))) {
+                    return false;
+                }
+                low++;
+                high--;
+            } else {
+                // Crossed threshold
+                break;
+            }
+        }
+        // If we get here, then it is valid
+        return true;
+    }
+    private boolean isValid(String s, int index) {
+        char c = s.charAt(index);
+        return (c >= '0' && c <= '9') || (c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z');
+    }
+    private char normalize(char c) {
+        if (c >= 'A' && c <= 'Z') {
+            return (char)(c - 'A' + 'a');
+        }
+        return c;
     }
 }
