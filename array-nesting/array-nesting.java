3fbb2675cbf0d9dee5a431f2cabd97523e780aea
sachin4429 <56739056+sachin4429@users.noreply.github.com>
2021-09-01 14:37:09 +0530

    Time: 1 ms (100.00%), Space: 40 MB (62.96%) - LeetHub

--- array-nesting/array-nesting.java
+++ array-nesting/array-nesting.java
@@ -1,21 +1,18 @@
-class Solution {
+public class Solution {
     public int arrayNesting(int[] nums) {
-        boolean[] visited = new boolean[nums.length];
-    int res = 0;
-
-    for (int i = 0; i < nums.length; i++) {
-      if (!visited[i]) {
-        int start = nums[i];
-        int count = 0;
-        do {
-          start = nums[start];
-          count++;
-          visited[start] = true;
+        int res = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != Integer.MAX_VALUE) {
+                int start = nums[i], count = 0;
+                while (nums[start] != Integer.MAX_VALUE) {
+                    int temp = start;
+                    start = nums[start];
+                    count++;
+                    nums[temp] = Integer.MAX_VALUE;
+                }
+                res = Math.max(res, count);
+            }
         }
-        while (start != nums[i]);
-        res = Math.max(res, count);
-      }
-    }
-    return res;
+        return res;
     }
 }
