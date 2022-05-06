36ca6e64f610a7cf7883d33c6520a9c9f49423cd
sachin4429 <56739056+sachin4429@users.noreply.github.com>
2022-05-06 21:41:14 +0530

    Time: 11 ms (93.78%), Space: 48.2 MB (70.73%) - LeetHub

--- 1209-remove-all-adjacent-duplicates-in-string-ii/1209-remove-all-adjacent-duplicates-in-string-ii.java
+++ 1209-remove-all-adjacent-duplicates-in-string-ii/1209-remove-all-adjacent-duplicates-in-string-ii.java
@@ -1,24 +1,22 @@
 class Solution {
-     public String removeDuplicates(String s, int k) {
-        
-        Stack<Character> charSt=new Stack<>();
-        Stack<Integer> countSt=new Stack<>();
-        
-        for(char ch:s.toCharArray()){
-            if(charSt.size()>0 && charSt.peek()==ch) countSt.push(countSt.peek()+1);
-            else countSt.push(1);
-            
-            charSt.push(ch);
-            if(countSt.peek()==k){
-                for(int i=0;i<k;i++){
-                    charSt.pop();
-                    countSt.pop();
+    public String removeDuplicates(String s, int k) {
+        int w = 0;
+        int n = s.length();
+        int []count = new int[n];
+        char[] ar = s.toCharArray();
+        for(int i=0;i<n;i++){
+            ar[w]=ar[i];          
+            if(w==0||ar[w-1]!=ar[w]){
+                count[w]=1;
+            }else{
+                count[w]=count[w-1]+1;
+                if(count[w]==k){
+                    w=w-k;
                 }
             }
+            w++;
         }
         
-        StringBuilder sb=new StringBuilder();
-        while(charSt.size()>0) sb.append(charSt.pop());
-        return sb.reverse().toString();
+        return new String(ar, 0, w);
     }
 }
