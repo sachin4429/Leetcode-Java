c9a85d41ad739ebdd438e2ad8b876bfd93754a86
sachin4429 <56739056+sachin4429@users.noreply.github.com>
2022-05-31 11:47:03 +0530

    Time: 200 ms (15.38%), Space: 70.9 MB (15.55%) - LeetHub

--- 18-4sum/18-4sum.java
+++ 18-4sum/18-4sum.java
@@ -1,32 +1,40 @@
 class Solution {
     public List<List<Integer>> fourSum(int[] nums, int target) {
-        List<List<Integer>> res = new ArrayList<>();
-        Arrays.sort(nums);
-        int n = nums.length;
-        for(int i=0;i<n-3;i++){
-            for(int j=i+1;j<n-2;j++){
-                int rem = target-(nums[i]+nums[j]);
-                int low = j+1,high = n-1;
-                while(low<high){
-                    int sum = nums[low]+nums[high];
-                    if(sum==rem){
-                        List<Integer> innerRes = new ArrayList<Integer>();
-                        innerRes.add(nums[i]);
-                        innerRes.add(nums[j]);
-                        innerRes.add(nums[low]);
-                        innerRes.add(nums[high]);
-                        if(!res.contains(innerRes)) 
-                            res.add(innerRes);
-                        low++;
-                        high--;
-                    }
-                    else if(sum>rem) 
-                        high--;
-                    else 
-                        low++;
-                }
-            }
+	List<List<Integer>> res = new ArrayList<List<Integer>>();
+	Arrays.sort(nums);
+	int n = nums.length;
+	if(n<4)
+		return res;
+	for(int i = 0; i<n-3; i++)
+	{
+		for(int j = i+1; j<n-2; j++)
+		{
+			int remain = target - nums[i]-nums[j];
+			int lo =j+1, hi = n-1;
+			while(lo<hi)
+			{
+				if(nums[lo]+nums[hi] == remain)
+				{
+					List<Integer> innerres = new ArrayList<Integer>();
+					innerres.add(nums[i]);
+					innerres.add(nums[j]);
+ 					innerres.add(nums[lo]);
+					innerres.add(nums[hi]);
+                    if(!res.contains(innerres))
+                        res.add(innerres);
+					lo++; hi--;
+				}
+				else if(nums[lo]+nums[hi] < remain)
+				{
+					lo++;
+				}
+				else 
+				{
+					hi--;
+				}
+        	}
         }
-        return res;
     }
+        return res;
+}
 }
