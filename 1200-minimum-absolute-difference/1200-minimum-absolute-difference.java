b03e7bc039bf5f796ca26d4b942851d15820db35
sachin4429 <56739056+sachin4429@users.noreply.github.com>
2022-06-04 07:54:42 +0530

    Time: 27 ms (28.73%), Space: 65.5 MB (26.65%) - LeetHub

--- 1200-minimum-absolute-difference/1200-minimum-absolute-difference.java
+++ 1200-minimum-absolute-difference/1200-minimum-absolute-difference.java
@@ -1,47 +1,26 @@
 class Solution {
     public List<List<Integer>> minimumAbsDifference(int[] arr) {
-        List<List <Integer>> ans=new ArrayList<>();
-        int min=Integer.MAX_VALUE ;
+        List<List<Integer>> res = new ArrayList<List<Integer>>();
+        int min = Integer.MAX_VALUE;
         Arrays.sort(arr);
-        for(int i=1;i<arr.length;i++){
-            int diff=(arr[i]-arr[i-1]);
-            min=Math.min(diff,min);
-        } 
-        for(int i=1;i<arr.length;i++){
-            if(arr[i]-arr[i-1]==min){
-                List<Integer> pair=new ArrayList<>();
-                pair.add(arr[i-1]);
-                pair.add(arr[i]);
-                ans.add(pair);
+        for(int i = 1; i<arr.length; i++)
+        {
+            List<Integer> innerres = new ArrayList<Integer>();
+            if(Math.abs(arr[i] - arr[i-1]) == min)
+            {
+                innerres.add(arr[i-1]);
+                innerres.add(arr[i]);
+                res.add(innerres);
+            }
+            else if(Math.abs(arr[i] - arr[i-1]) < min)
+            {
+                min = Math.abs(arr[i] - arr[i-1]);
+                res.clear();
+                innerres.add(arr[i-1]);
+                innerres.add(arr[i]);
+                res.add(innerres);
             }
         }
-        return ans;
+        return res;
     }
 }
-
-// class Solution {
-//     public List<List<Integer>> minimumAbsDifference(int[] arr) {
-//         List<List<Integer>> res = new ArrayList<List<Integer>>();
-//         int min = Integer.MAX_VALUE;
-//         Arrays.sort(arr);
-//         for(int i = 1; i<arr.length; i++)
-//         {
-//             List<Integer> innerres = new ArrayList<Integer>();
-//             if(Math.abs(arr[i] - arr[i-1]) == min)
-//             {
-//                 innerres.add(arr[i-1]);
-//                 innerres.add(arr[i]);
-//                 res.add(innerres);
-//             }
-//             else if(Math.abs(arr[i] - arr[i-1]) < min)
-//             {
-//                 min = Math.abs(arr[i] - arr[i-1]);
-//                 res.clear();
-//                 innerres.add(arr[i-1]);
-//                 innerres.add(arr[i]);
-//                 res.add(innerres);
-//             }
-//         }
-//         return res;
-//     }
-// }
