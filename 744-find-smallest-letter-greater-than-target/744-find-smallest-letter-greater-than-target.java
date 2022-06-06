318ee7ae5d12d92c44c274ab5e4f27e427029c42
sachin4429 <56739056+sachin4429@users.noreply.github.com>
2022-06-06 19:51:47 +0530

    Time: 1 ms (37.88%), Space: 48.2 MB (35.70%) - LeetHub

--- 744-find-smallest-letter-greater-than-target/744-find-smallest-letter-greater-than-target.java
+++ 744-find-smallest-letter-greater-than-target/744-find-smallest-letter-greater-than-target.java
@@ -1,23 +1,41 @@
 class Solution {
-    public char nextGreatestLetter(char[] letters, char target) {
-        int lo = 0, hi = letters.length;
-        if(letters[hi-1]<= target)
-            return letters[0];
-        while(lo<=hi)
-        {
-            int mid = lo + (hi-lo)/2;
-            if(letters[mid] == target)
-            {
-                if(letters[(mid+1)%letters.length] != letters[mid])
-                    return letters[(mid+1)%letters.length];
-                else
-                    lo = mid+1;
-            }
-            else if(letters[mid] < target)
-                lo = mid+1;
-            else
-                hi = mid-1;
+    public char nextGreatestLetter(char[] a, char x) {
+       int n = a.length;
+       
+        if (x >= a[n - 1])   x = a[0];
+        else    x++;
+        
+        int lo = 0, hi = n - 1;
+        while (lo < hi) {
+            int mid = lo + (hi - lo) / 2;
+            if (a[mid] == x)    return a[mid];
+            if (a[mid] < x)     lo = mid + 1;
+            else    hi = mid;
         }
-        return letters[hi+1];
+        return a[hi];
     }
 }
+
+// class Solution {
+//     public char nextGreatestLetter(char[] letters, char target) {
+//         int lo = 0, hi = letters.length;
+//         if(letters[hi-1]<= target)
+//             return letters[0];
+//         while(lo<=hi)
+//         {
+//             int mid = lo + (hi-lo)/2;
+//             if(letters[mid] == target)
+//             {
+//                 if(letters[(mid+1)%letters.length] != letters[mid])
+//                     return letters[(mid+1)%letters.length];
+//                 else
+//                     lo = mid+1;
+//             }
+//             else if(letters[mid] < target)
+//                 lo = mid+1;
+//             else
+//                 hi = mid-1;
+//         }
+//         return letters[hi+1];
+//     }
+// }
