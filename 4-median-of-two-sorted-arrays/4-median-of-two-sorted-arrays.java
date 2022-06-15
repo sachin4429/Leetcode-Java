67fa63351e7d5cbf0082395cc8849d4b80e30412
sachin4429 <56739056+sachin4429@users.noreply.github.com>
2022-06-15 12:51:27 +0530

    Time: 4 ms (61.61%), Space: 49.9 MB (42.31%) - LeetHub

--- 4-median-of-two-sorted-arrays/4-median-of-two-sorted-arrays.java
+++ 4-median-of-two-sorted-arrays/4-median-of-two-sorted-arrays.java
@@ -1,42 +1,28 @@
 class Solution {
-    public double findMedianSortedArrays(int[] nums1, int[] nums2) {
-        int n =nums1.length;
-        int m = nums2.length;
-        int arr[] = new int[m+n];
-        int i = 0, j = 0, k = 0;
-        while(i<n && j<m)
-        {
-            if(nums1[i] == nums2[j])
-            {
-                arr[k++] = nums1[i++];
-                arr[k++] = nums2[j++];
-            }
-            else if(nums1[i]<nums2[j])
-            {
-                arr[k++] = nums1[i++];
-            }
-            else
-            {
-                arr[k++] = nums2[j++];
-            }
+    public double findMedianSortedArrays(int[] arr1, int[] arr2) {
+        int m = arr1.length, n = arr2.length;
+        if(m>n)
+        return findMedianSortedArrays(arr2,arr1);//ensuring that binary search happens on minimum size array
+        
+    int low=0,high=m,medianPos=((m+n)+1)/2;
+    while(low<=high) {
+        int cut1 = (low+high)>>1;
+        int cut2 = medianPos - cut1;
+        
+        int l1 = (cut1 == 0)? Integer.MIN_VALUE:arr1[cut1-1];
+        int l2 = (cut2 == 0)? Integer.MIN_VALUE:arr2[cut2-1];
+        int r1 = (cut1 == m)? Integer.MAX_VALUE:arr1[cut1];
+        int r2 = (cut2 == n)? Integer.MAX_VALUE:arr2[cut2];
+        
+        if(l1<=r2 && l2<=r1) {
+            if((m+n)%2 != 0)
+                return Math.max(l1,l2);
+            else 
+                return (Math.max(l1,l2)+Math.min(r1,r2))/2.0;
         }
-        while(j<m)
-        {
-            arr[k++] = nums2[j++];
-        }
-        while(i<n)
-        {
-            arr[k++] = nums1[i++];
-        }
-        for(int x : arr)
-        {
-            System.out.print(x+" ");
-        }
-        double result = 0.0;
-        if((n+m)%2==0)
-            result = (double)(arr[(n+m)/2] + arr[(n+m)/2-1]) / 2;
-        else 
-            result = arr[(n+m)/2];
-        return result;
+        else if(l1>r2) high = cut1-1;
+        else low = cut1+1;
+    }
+    return 0.0;
     }
 }
