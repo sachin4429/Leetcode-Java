3fde4c2c9f5db270a8d56f221a54281f3fa25569
sachin4429 <56739056+sachin4429@users.noreply.github.com>
2022-06-23 17:07:35 +0530

    Time: 1 ms (91.30%), Space: 41.3 MB (48.67%) - LeetHub

--- 2310-sum-of-numbers-with-units-digit-k/2310-sum-of-numbers-with-units-digit-k.java
+++ 2310-sum-of-numbers-with-units-digit-k/2310-sum-of-numbers-with-units-digit-k.java
@@ -2,8 +2,6 @@ class Solution {
     public int minimumNumbers(int num, int k) {
         if(num == 0)
             return 0;
-        if(k == 0 && num % 10 == 0)
-                return 1;
         if(k>num)
             return -1;
         if(num%2 != 0 && k%2 == 0)
