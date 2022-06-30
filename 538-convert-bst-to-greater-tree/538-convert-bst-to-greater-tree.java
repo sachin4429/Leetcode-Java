130c0af2b01dda64d2b94027b5151269b4da0678
sachin4429 <56739056+sachin4429@users.noreply.github.com>
2022-06-30 08:20:15 +0530

    Time: 1 ms (86.51%), Space: 49.6 MB (47.56%) - LeetHub

--- 538-convert-bst-to-greater-tree/538-convert-bst-to-greater-tree.java
+++ 538-convert-bst-to-greater-tree/538-convert-bst-to-greater-tree.java
@@ -14,37 +14,56 @@
  * }
  */
 class Solution {
-    class Pair
-    {
-        TreeNode node;
-        int data;
-        Pair(TreeNode node, int data)
-        {
-            this.node = node;
-            this.data = data;
-        }
-    }
-    ArrayList<Pair> arr = new ArrayList<Pair>();
-    public void inorder(TreeNode root)
-    {
-        if(root == null)
-            return;
-        inorder(root.left);
-        arr.add(new Pair(root, root.val));
-        inorder(root.right);
-    }
-    public TreeNode convertBST(TreeNode root)
-    {
-        if(root == null || (root.left == null && root.right == null))
-            return root;
-        
-        inorder(root);
-        int last = arr.get(arr.size()-1).data;
-        for(int i = arr.size()-2; i>=0; i--)
-        {
-            arr.get(i).node.val += last;
-            last = arr.get(i).node.val;
-        }
+    public TreeNode convertBST(TreeNode root) {
+        traverse(root);
         return root;
     }
+    
+    private int sum = 0;
+    
+    private void traverse(TreeNode root) {
+        if(root == null) {
+            return;
+        }
+        
+        traverse(root.right);
+        sum += root.val;
+        root.val = sum;
+        traverse(root.left);
+    }
 }
+// class Solution {
+//     class Pair
+//     {
+//         TreeNode node;
+//         int data;
+//         Pair(TreeNode node, int data)
+//         {
+//             this.node = node;
+//             this.data = data;
+//         }
+//     }
+//     ArrayList<Pair> arr = new ArrayList<Pair>();
+//     public void inorder(TreeNode root)
+//     {
+//         if(root == null)
+//             return;
+//         inorder(root.left);
+//         arr.add(new Pair(root, root.val));
+//         inorder(root.right);
+//     }
+//     public TreeNode convertBST(TreeNode root)
+//     {
+//         if(root == null || (root.left == null && root.right == null))
+//             return root;
+        
+//         inorder(root);
+//         int last = arr.get(arr.size()-1).data;
+//         for(int i = arr.size()-2; i>=0; i--)
+//         {
+//             arr.get(i).node.val += last;
+//             last = arr.get(i).node.val;
+//         }
+//         return root;
+//     }
+// }
