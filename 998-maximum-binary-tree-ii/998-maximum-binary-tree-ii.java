ab11e8053013642f334b949072e964381e2b600d
sachin4429 <56739056+sachin4429@users.noreply.github.com>
2022-07-13 15:07:07 +0530

    Time: 0 ms (100.00%), Space: 42 MB (56.85%) - LeetHub

--- 998-maximum-binary-tree-ii/998-maximum-binary-tree-ii.java
+++ 998-maximum-binary-tree-ii/998-maximum-binary-tree-ii.java
@@ -13,44 +13,67 @@
  *     }
  * }
  */
+
 class Solution {
-    ArrayList<Integer> arr = new ArrayList<Integer>(); 
     public TreeNode insertIntoMaxTree(TreeNode root, int val) {
-        if(root == null)
-            return null;
-        inorder(root);
-        arr.add(val);
-        return buildTree(arr, 0, arr.size()-1);
-        
-    }
-    public void inorder(TreeNode root)
-    {
-        if(root == null)
-            return;
-        inorder(root.left);
-        arr.add(root.val);
-        inorder(root.right);
-    }
-    public TreeNode buildTree(ArrayList<Integer> arr, int si, int ei)
-    {
-        if(si>ei)
-            return null;
-        int max = findMax(arr, si, ei);
-        TreeNode root = new TreeNode(arr.get(max));
-        root.left = buildTree(arr,si,max-1);
-        root.right = buildTree(arr,max+1,ei);
+
+        TreeNode addedNode = new TreeNode(val);
+        TreeNode curNode = root;
+        // addedNode is the new root:
+        if(val > curNode.val){
+            addedNode.left = curNode;
+            return addedNode;
+        }        
+        // root is still the root:
+            // curNode.val > val > curNode.right.val 
+            // curNode.val > val, curNode.right is null
+        while(curNode.right != null && curNode.right.val > val){
+            curNode = curNode.right;
+        }
+        addedNode.left = curNode.right;
+        curNode.right = addedNode;
         return root;
     }
-    public int findMax(ArrayList<Integer> arr, int si, int ei)
-    {
-        int max = Integer.MIN_VALUE;
-        int ind = 0;
-        for(int i = si; i<=ei; i++)
-            if(max<arr.get(i))
-            {
-                max = arr.get(i);
-                ind = i;
-            }
-        return ind;
-    }
 }
+
+// class Solution {
+//     ArrayList<Integer> arr = new ArrayList<Integer>(); 
+//     public TreeNode insertIntoMaxTree(TreeNode root, int val) {
+//         if(root == null)
+//             return null;
+//         inorder(root);
+//         arr.add(val);
+//         return buildTree(arr, 0, arr.size()-1);
+        
+//     }
+//     public void inorder(TreeNode root)
+//     {
+//         if(root == null)
+//             return;
+//         inorder(root.left);
+//         arr.add(root.val);
+//         inorder(root.right);
+//     }
+//     public TreeNode buildTree(ArrayList<Integer> arr, int si, int ei)
+//     {
+//         if(si>ei)
+//             return null;
+//         int max = findMax(arr, si, ei);
+//         TreeNode root = new TreeNode(arr.get(max));
+//         root.left = buildTree(arr,si,max-1);
+//         root.right = buildTree(arr,max+1,ei);
+//         return root;
+//     }
+//     public int findMax(ArrayList<Integer> arr, int si, int ei)
+//     {
+//         int max = Integer.MIN_VALUE;
+//         int ind = 0;
+//         for(int i = si; i<=ei; i++)
+//             if(max<arr.get(i))
+//             {
+//                 max = arr.get(i);
+//                 ind = i;
+//             }
+//         return ind;
+//     }
+// }
