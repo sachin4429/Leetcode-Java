33cfe0dd774c59c426297e127b05d915299811ed
sachin4429 <56739056+sachin4429@users.noreply.github.com>
2022-07-20 22:41:07 +0530

    Time: 1 ms (97.96%), Space: 40.4 MB (95.82%) - LeetHub

---
+++ 2259-remove-digit-from-number-to-maximize-result/2259-remove-digit-from-number-to-maximize-result.java
@@ -0,0 +1,15 @@
+class Solution {
+    public String removeDigit(String number, char digit) {
+        for(int i = 0; i<number.length()-2; i++)
+        {
+            if(number.charAt(i) == digit && number.charAt(i) < number.charAt(i+1))
+                return number.substring(0, i)+number.substring(i+1);
+        }
+        for(int i = number.length()-1; i>=0; i--)
+        {
+            if(number.charAt(i) == digit)
+                return number.substring(0, i)+number.substring(i+1);
+        }
+        return "";
+    }
+}
