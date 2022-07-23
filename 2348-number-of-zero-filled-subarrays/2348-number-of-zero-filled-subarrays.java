c8af4514534c253d58a743f16dfe7ba2a1ee5a05
sachin4429 <56739056+sachin4429@users.noreply.github.com>
2022-07-23 21:44:19 +0530

    Time: 3 ms (100.00%), Space: 59.9 MB (100.00%) - LeetHub

--- 2348-number-of-zero-filled-subarrays/2348-number-of-zero-filled-subarrays.java
+++ 2348-number-of-zero-filled-subarrays/2348-number-of-zero-filled-subarrays.java
@@ -1,32 +1,45 @@
 class Solution {
     public long zeroFilledSubarray(int[] nums) {
-        int j = 0;
-        long count = 0;
-        ArrayList<Long> arr = new ArrayList<Long>();
-        for(int i : nums)
+        long ans=0,l=0;
+        for(int x:nums)
         {
-            if(i == 0)
-                count++;
-            else
-            {
-                if(count != 0)
-                    arr.add(count);
-                count = 0;
-            }
-        }
-        arr.add(count);
-        long ans = 0;
-        for(Long i : arr)
-        {
-            if(i%2 == 0)
-            {
-                ans += (i/2)*(i+1);
-            }
-            else
-            {
-                ans += (i)*((i+1)/2);
-            }
+            if(x==0) l++;
+            else l=0;
+            ans+=l;
         }
         return ans;
     }
 }
+
+// class Solution {
+//     public long zeroFilledSubarray(int[] nums) {
+//         int j = 0;
+//         long count = 0;
+//         ArrayList<Long> arr = new ArrayList<Long>();
+//         for(int i : nums)
+//         {
+//             if(i == 0)
+//                 count++;
+//             else
+//             {
+//                 if(count != 0)
+//                     arr.add(count);
+//                 count = 0;
+//             }
+//         }
+//         arr.add(count);
+//         long ans = 0;
+//         for(Long i : arr)
+//         {
+//             if(i%2 == 0)
+//             {
+//                 ans += (i/2)*(i+1);
+//             }
+//             else
+//             {
+//                 ans += (i)*((i+1)/2);
+//             }
+//         }
+//         return ans;
+//     }
+// }
