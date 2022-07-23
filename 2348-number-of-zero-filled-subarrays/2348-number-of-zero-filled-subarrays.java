99c9d8fbaa1a425c52ddb0fc90f698d356a5b8aa
sachin4429 <56739056+sachin4429@users.noreply.github.com>
2022-07-23 21:41:36 +0530

    Time: 10 ms (20.00%), Space: 99.1 MB (40.00%) - LeetHub

---
+++ 2348-number-of-zero-filled-subarrays/2348-number-of-zero-filled-subarrays.java
@@ -0,0 +1,32 @@
+class Solution {
+    public long zeroFilledSubarray(int[] nums) {
+        int j = 0;
+        long count = 0;
+        ArrayList<Long> arr = new ArrayList<Long>();
+        for(int i : nums)
+        {
+            if(i == 0)
+                count++;
+            else
+            {
+                if(count != 0)
+                    arr.add(count);
+                count = 0;
+            }
+        }
+        arr.add(count);
+        long ans = 0;
+        for(Long i : arr)
+        {
+            if(i%2 == 0)
+            {
+                ans += (i/2)*(i+1);
+            }
+            else
+            {
+                ans += (i)*((i+1)/2);
+            }
+        }
+        return ans;
+    }
+}
