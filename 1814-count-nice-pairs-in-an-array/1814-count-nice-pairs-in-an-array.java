72929a25cfd506a2647da8448d9d494e879f6a0a
sachin4429 <56739056+sachin4429@users.noreply.github.com>
2022-08-09 21:44:14 +0530

    Time: 490 ms (5.09%), Space: 121.8 MB (5.55%) - LeetHub

--- 1814-count-nice-pairs-in-an-array/1814-count-nice-pairs-in-an-array.java
+++ 1814-count-nice-pairs-in-an-array/1814-count-nice-pairs-in-an-array.java
@@ -1,35 +1,28 @@
 class Solution {
     public int countNicePairs(int[] nums) {
         HashMap<Long, Integer> map = new HashMap<>();
-        int count = 0;
-        int mod=(int)Math.pow(10,9)+7;
+        int res = 0;
         for(int i = 0; i<nums.length; i++)
         {
-            long rev=rev(nums[i]);
-            
-            long diff=nums[i]-rev;
-            if(map.containsKey(diff)){
-                count=(count%mod + map.get(diff)%mod)%mod;
+            //System.out.println((long)nums[i]-rev(nums[i]));
+            if(map.containsKey((long)nums[i]-rev(nums[i])))
+            {
+                map.put((long)nums[i]-rev(nums[i]), map.get((long)nums[i]-rev(nums[i]))+1);
+                res = (res%1000000007 +map.get((long)nums[i]-rev(nums[i]))%1000000007)%1000000007;
+                //System.out.println(res);
             }
-            
-            map.put(diff,map.getOrDefault(diff,0)+1);
+            else
+                map.put((long)nums[i]-rev(nums[i]),0);
         }
-        return  count%mod;
+        int n = nums.length;
+        return  res%1000000007;
     }
-    // public long rev(int x)
-    // {
-    //     StringBuilder sb = new StringBuilder(x+"");
-    //     sb.reverse();
-    //     while(sb.charAt(0) == 0)
-    //         sb.delete(0,1);
-    //     return Long.parseLong(sb.toString());
-    // }
-    private long rev(int num){
-        long rev=0;
-        while(num!=0){
-            rev=rev*10 + num%10;
-            num/=10;
-        }
-        return rev;
+    public long rev(int x)
+    {
+        StringBuilder sb = new StringBuilder(x+"");
+        sb.reverse();
+        while(sb.charAt(0) == 0)
+            sb.delete(0,1);
+        return Long.parseLong(sb.toString());
     }
 }
