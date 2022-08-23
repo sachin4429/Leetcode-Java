fc337e31f9dbf30e4605c47c832eea84c870fbea
sachin4429 <56739056+sachin4429@users.noreply.github.com>
2022-08-23 10:03:16 +0530

    Time: 6 ms (79.16%), Space: 97.3 MB (64.12%) - LeetHub

--- 234-palindrome-linked-list/234-palindrome-linked-list.java
+++ 234-palindrome-linked-list/234-palindrome-linked-list.java
@@ -28,6 +28,7 @@ class Solution {
             return true;
         var fast = head; 
         var slow = head;
+        
         while(fast != null && fast.next != null)
         {
             fast = fast.next.next;
